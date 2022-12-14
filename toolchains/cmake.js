1d966e1191c5700d02c59fd1ee24b0aac2e79584
Calin Capitanu <calincapitanu@gmail.com>
2022-12-14 20:56:11 +0100

    fix: indentation

--- toolchains/cmake.js
+++ toolchains/cmake.js
@@ -1,74 +1,70 @@
-const impl = ctx => {
-	const { version, sha1_aarch64, sha1_x86_64 } = ctx.cfg();
+const impl = (ctx) => {
+  const { version, sha1_aarch64, sha1_x86_64 } = ctx.cfg();
 
-	const { host } = ctx.env();
+  const { host } = ctx.env();
 
+  let os = host.os;
+  if (os == "darwin") {
+    os = "macos";
+  }
+  if (os == "win32") {
+    os = "windows";
+  }
 
-	let os = host.os;
-	if (os == "darwin") { os = "macos"; }
-	if (os == "win32") { os = "windows"; }
+  let arch = host.arch;
+  if (os == "macos") {
+    arch = "universal";
+  }
 
-	let arch = host.arch;
-	if (os == "macos") { arch = "universal"; }
+  let ext = "tar.gz";
+  if (os == "win32") {
+    ext = "zip";
+  }
 
-	let ext = "tar.gz";
-	if (os == "win32") { ext = "zip" };
+  let sha1 = sha1_aarch64;
+  if (arch == "x86_64") {
+    sha1 = sha1_x86_64;
+  }
 
-	let sha1 = sha1_aarch64
-	if (arch == "x86_64") {
-		sha1 = sha1_x86_64
-	}
+  const cmake = `cmake-${version}-${os}-${arch}`;
+  const url = `https://github.com/Kitware/CMake/releases/download/v${version}/${cmake}.${ext}`;
 
-	
+  const output = `cmake.${ext}`;
+  ctx.action().download({ url, sha1, output });
 
+  ctx.action().extract({ src: output, dst: "." });
 
-	const cmake = `cmake-${version}-${os}-${arch}`;
-	const url = `https://github.com/Kitware/CMake/releases/download/v${version}/${cmake}.${ext}`
+  if (os == "macos") {
+    ctx.action().declareOutputs([`${cmake}/CMake.app/Contents/bin`]);
 
-	const output = `cmake.${ext}`
-	ctx.action().download({ url, sha1, output })
+    ctx.provides({
+      cmake: `${cmake}/CMake.app/Contents/bin/cmake`,
+      cpack: `${cmake}/CMake.app/Contents/bin/cpack`,
+      ctest: `${cmake}/CMake.app/Contents/bin/ctest`,
+      ccmake: `${cmake}/CMake.app/Contents/bin/ccmake`,
+    });
+  }
 
-	ctx.action().extract({ src: output, dst: "." })
+  if (os == "linux") {
+    ctx.action().declareOutputs([`${cmake}/bin`]);
 
-
-	if (os == "macos") {
-		ctx.action().declareOutputs([
-			`${cmake}/CMake.app/Contents/bin`,
-		]);
-
-		ctx.provides({
-			cmake: `${cmake}/CMake.app/Contents/bin/cmake`,
-			cpack: `${cmake}/CMake.app/Contents/bin/cpack`,
-			ctest: `${cmake}/CMake.app/Contents/bin/ctest`,
-			ccmake: `${cmake}/CMake.app/Contents/bin/ccmake`,
-		});
-	}
-
-	if (os == "linux") {
-
-		ctx.action().declareOutputs([
-			`${cmake}/bin`,
-		]);
-
-		ctx.provides({
-			cmake: `${cmake}/bin/cmake`,
-			cpack: `${cmake}/bin/cpack`,
-			ctest: `${cmake}/bin/ctest`,
-			ccmake: `${cmake}/bin/ccmake`,			
-		})
-	}
-}
-
+    ctx.provides({
+      cmake: `${cmake}/bin/cmake`,
+      cpack: `${cmake}/bin/cpack`,
+      ctest: `${cmake}/bin/ctest`,
+      ccmake: `${cmake}/bin/ccmake`,
+    });
+  }
+};
 
 export default Warp.Toolchain({
-	name: "https://rules.warp.build/toolchains/cmake",
-	mnemonic: "CMake",
-	impl,
-	cfg: {
-		version: string(),
-		sha1_aarch64: string(),
-		sha1_x86_64: string(),
-	},
-	toolchains: []
+  name: "https://rules.warp.build/toolchains/cmake",
+  mnemonic: "CMake",
+  impl,
+  cfg: {
+    version: string(),
+    sha1_aarch64: string(),
+    sha1_x86_64: string(),
+  },
+  toolchains: [],
 });
-
--- toolchains/protobuf.js
+++ toolchains/protobuf.js
@@ -1,37 +1,37 @@
 export const PROTO_EXT = ".proto";
 
-const impl = ctx => {
+const impl = (ctx) => {
   const env = ctx.env();
   const cfg = ctx.cfg();
 
-	let arch = env.host.arch
-	
-	if (arch == "aarch64") {
-		arch = "aarch_64"
-	}
-	
-	let sha1 = cfg.sha1_aarch_64
-	if (arch == "x86_64") {
-		sha1 = cfg.sha1_x86_64
-	}
+  let arch = env.host.arch;
 
-	trace(env.host.arch)
-
-  let os = env.host.os
-  if (os == "darwin") {
-    os = "osx"
+  if (arch == "aarch64") {
+    arch = "aarch_64";
   }
 
-	const url = `https://github.com/protocolbuffers/protobuf/releases/download/v${cfg.version}/protoc-${cfg.version}-${os}-${arch}.zip`
+  let sha1 = cfg.sha1_aarch_64;
+  if (arch == "x86_64") {
+    sha1 = cfg.sha1_x86_64;
+  }
 
-  const output = `protoc-${cfg.version}.zip`
+  trace(env.host.arch);
 
-  ctx.action().download({ url, sha1, output })
+  let os = env.host.os;
+  if (os == "darwin") {
+    os = "osx";
+  }
 
-  ctx.action().extract({ src: output, dst: "." })
+  const url = `https://github.com/protocolbuffers/protobuf/releases/download/v${cfg.version}/protoc-${cfg.version}-${os}-${arch}.zip`;
+
+  const output = `protoc-${cfg.version}.zip`;
+
+  ctx.action().download({ url, sha1, output });
+
+  ctx.action().extract({ src: output, dst: "." });
 
   const protoc = `bin/protoc`;
-  ctx.action().setPermissions({ file: protoc, executable: true })
+  ctx.action().setPermissions({ file: protoc, executable: true });
   ctx.action().declareOutputs(["."]);
   ctx.provides({ protoc });
 };
@@ -43,6 +43,6 @@ export default Warp.Toolchain({
   cfg: {
     version: string(),
     sha1_aarch64: string(),
-	  sha1_x86_64: string(),
-  }
+    sha1_x86_64: string(),
+  },
 });
